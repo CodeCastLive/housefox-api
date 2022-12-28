114eb6ae1259b4fd6cb281473ed36fe0bbc0ddb4
thisiscolinmorgan <thisiscolinmorgan@gmail.com>
2022-12-28 15:29:50 +0500

    initial spring files with gradle

---
+++ src/main/java/com/example/HousefoxApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousefoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousefoxApplication.class, args);
+	}
+
+}
